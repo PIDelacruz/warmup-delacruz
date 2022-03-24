82d5db9421d0282ea7dbc582360f72ff40f4d7c2
PIDelacruz <101232440+PIDelacruz@users.noreply.github.com>
2022-03-24 11:18:09 +0800

    database sql file
    database sql

--- database/warmup.sql
+++ database/warmup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2022 at 09:41 AM
+-- Generation Time: Mar 24, 2022 at 04:17 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(150) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `is_admin` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`user_id`, `username`, `password`, `is_admin`) VALUES
+(1, 'admin', '$2y$10$h3JRRqh6lLgPRXivDlgE8uQKkxR8wq3uJytvRTTGiGnvlr0dW7lNe', 1),
+(2, 'user', '$2y$10$/NPYbTfHjsTKyF76o2EnfeYhRAhmNZoKLzVJRovnLk7fyL3LWAHZC', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `messages`
 --
 
@@ -38,11 +59,18 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `message`, `mess_datetime`) VALUES
-(12, 'test', '2022-03-16 16:40:50');
+(12, 'test', '2022-03-16 16:40:50'),
+(13, 'test', '2022-03-24 10:40:20');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Indexes for table `messages`
@@ -55,10 +83,16 @@ ALTER TABLE `messages`
 --
 
 --
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
